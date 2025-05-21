573aa6adb83187e48cebadcb1f1f26d4db7131f9
Pooja-game-dev <pooja@freebirdanimation.com>
2025-05-21 15:31:13 +0530

    Update AnimationPlay.cs

--- Assets/Assets/Scripts/MainMenu/AnimationPlay.cs
+++ Assets/Assets/Scripts/MainMenu/AnimationPlay.cs
@@ -4,12 +4,13 @@ using DG.Tweening;
 public class AnimationPlay : MonoBehaviour
 {
      [SerializeField] float duration=.3f;
-    Vector3 target = new Vector3(1, 1, 0);  
+    Vector3 target = new Vector3(1, 1, 0);
 
-     public void ScaleUpanim()
+    public void ScaleUpanim()
     {
-        
+
         transform.DOScale(target, duration);
+        Debug.Log("this is try");
     }
 
 }
